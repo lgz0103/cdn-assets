2ab31d28bf3a4ae7479b640009b075a896bd0a72
lgz0103 <2841573915@qq.com>
2022-08-20 03:57:03 +0800

    1.0.1

--- js/hititle.js
+++ js/hititle.js
@@ -3,12 +3,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
+         document.title = 'ヽ(●-`Д´-)ノ人呢？';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
+         document.title = 'ヾ(Ő∀Ő3)ノ' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
